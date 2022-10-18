c45a07bcc5fa2a0dd52079e779bd7f8643307c13
hoty <iiec1337@yandex.ru>
2022-10-18 14:36:43 +0300

    remove try-catch

--- src/main/java/servlet/AreaCheckServlet.java
+++ src/main/java/servlet/AreaCheckServlet.java
@@ -1,11 +1,9 @@
 package servlet;
 
 import data.Point;
-import exception.IncorrectDataException;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
@@ -13,9 +11,13 @@ import javax.servlet.annotation.*;
 
 @WebServlet(name = "AreaCheckServlet", value = "/check")
 public class AreaCheckServlet extends HttpServlet {
+    private HttpServletRequest request;
+    private HttpServletResponse response;
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         resp.setContentType("text/html");
+        request = req;
+        response = resp;
         long startTime = System.nanoTime();
 
         String x = req.getParameter("x");
@@ -23,65 +25,80 @@ public class AreaCheckServlet extends HttpServlet {
         String r = req.getParameter("r");
         double xValue, yValue, rValue;
 
-        try {
-            if(x == null || x.equals("")) throw new IncorrectDataException("x is not set");
-            if(y == null || y.equals("")) throw new IncorrectDataException("y is not set");
-            if(r == null || r.equals("")) throw new IncorrectDataException("r is not set");
-
-            xValue = parseX(x.trim().replace("'", ""));
-            yValue = parseY(y.trim().replace("'", ""));
-            rValue = parseR(r.trim().replace("'", ""));
-
-            boolean isInside = isInsideCircle(xValue, yValue, rValue) ||
-                    isInsideTriangle(xValue, yValue, rValue) ||
-                    isInsideSquare(xValue, yValue, rValue);
-
-            long time = System.currentTimeMillis() / 1000000;
-
-            String execTime = String.format("%.9f", (System.nanoTime() - startTime) / 1000000000.0);
-
-            Object obj = req.getSession().getAttribute("table");
-            List<Point> table;
-            if(obj != null) {
-                table = (List<Point>) obj;
-            } else {
-                table = new ArrayList<>();
-            }
-            Point pnt = new Point(xValue, yValue, rValue, execTime, isInside, time);
-            table.add(pnt);
-            req.getSession().setAttribute("table", table);
-            req.getSession().setAttribute("point", pnt);
-
-            getServletContext().getRequestDispatcher("/results.jsp").forward(req, resp);
-
-        } catch (IncorrectDataException e) {
-            req.setAttribute("err_msg", e.getMessage());
-            getServletContext().getRequestDispatcher("/bad_data.jsp").forward(req, resp);
+        if(x == null || x.equals(""))  {
+            error("x is not set");
+            return;
         }
+        if(y == null || y.equals("")) {
+            error("y is not set");
+            return;
+        }
+        if(r == null || r.equals("")) {
+            error("r is not set");
+            return;
+        }
+
+        xValue = parseX(x.trim().replace("'", ""));
+        yValue = parseY(y.trim().replace("'", ""));
+        rValue = parseR(r.trim().replace("'", ""));
+
+        boolean isInside = isInsideCircle(xValue, yValue, rValue) ||
+                isInsideTriangle(xValue, yValue, rValue) ||
+                isInsideSquare(xValue, yValue, rValue);
+
+        long time = System.currentTimeMillis() / 1000000;
+
+        String execTime = String.format("%.9f", (System.nanoTime() - startTime) / 1000000000.0);
+
+        Object obj = req.getSession().getAttribute("table");
+        List<Point> table = new ArrayList<>();
+        if(obj != null) {
+            table = (List<Point>) obj;
+        }
+        Point pnt = new Point(xValue, yValue, rValue, execTime, isInside, time);
+        table.add(pnt);
+        req.getSession().setAttribute("table", table);
+        req.getSession().setAttribute("point", pnt);
+
+        getServletContext().getRequestDispatcher("/results.jsp").forward(req, resp);
     }
 
-    private double parseX(String x) throws IncorrectDataException {
+    private double parseX(String x) throws ServletException, IOException {
         double xTemp;
         xTemp = Double.parseDouble(x);
-        if(xTemp < -5 || xTemp > 3) throw new IncorrectDataException("x(" + xTemp + ") is out of range [-5;3]");
+        if(xTemp < -5 || xTemp > 3) {
+            error("x(" + xTemp + ") is out of range [-5;3]");
+            return -111;
+        }
 
         return xTemp;
     }
 
-    private double parseY(String y) throws IncorrectDataException {
+    private double parseY(String y) throws ServletException, IOException {
         double yTemp;
         yTemp = Double.parseDouble(y);
-        if(yTemp < -3 || yTemp > 3) throw new IncorrectDataException("y(" + yTemp + ") is out of range [-3;3]");
+        if(yTemp < -3 || yTemp > 3) {
+            error("y(" + yTemp + ") is out of range [-3;3]");
+            return -111;
+        }
 
         return yTemp;
     }
 
-    private double parseR(String r) throws IncorrectDataException {
+    private double parseR(String r) throws ServletException, IOException {
         double rTemp;
         rTemp = Double.parseDouble(r);
-        if(rTemp < 1 || rTemp > 3) throw new IncorrectDataException("r(" + rTemp + ") is out of range [1;3]");
+        if(rTemp < 1 || rTemp > 3) {
+            error("r(" + rTemp + ") is out of range [1;3]");
+            return -111;
+        }
 
         return rTemp;
+    }
+
+    private void error(String r) throws ServletException, IOException {
+        request.setAttribute("err_msg", r);
+        getServletContext().getRequestDispatcher("/bad_data.jsp").forward(request, response);
     }
 
     private boolean isInsideCircle(Double x, Double y, Double r) {
